f2bba0381ac5c61c8b12991a348c87ba58a0c93c
aswarda <97010938+aswarda@users.noreply.github.com>
2025-01-06 13:43:54 +0530

    Create eks_cluster_2.tf

---
+++ eks_cluster_2.tf
@@ -0,0 +1,32 @@
+resource "aws_eks_cluster" "eks_cluster2" {
+  name     = "my-eks-cluster-2"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.private_subnet_1.id,
+      aws_subnet.private_subnet_2.id
+    ]
+    security_group_ids = [aws_security_group.eks_security_group.id]
+  }
+}
+
+resource "aws_eks_node_group" "my_node_group2" {
+  cluster_name    = aws_eks_cluster.eks_cluster2.name
+  node_group_name = "my-node-group2"
+  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+
+  subnet_ids = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+  ami_type       = "AL2_x86_64"
+}
