0ebc908c2ba2a5e592f9434c3af645992b0fd1f1
aswarda <97010938+aswarda@users.noreply.github.com>
2026-08-26 19:03:39 +0530

    Update dummy_secret.py

--- dummy_secret.py
+++ dummy_secret.py
@@ -1,3 +1,3 @@
 # dummy_secret.py — for testing gitleaks locally, DELETE before real commit
-aws_access_key_id = "AKIAABCDEFGHIJKLMNOP"
+aws_access_key_id = "ABCDEAKIAABCDEFGHIJKLMNOP"
 aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/dummy/testkey/abc123"
